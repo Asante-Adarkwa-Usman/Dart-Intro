11795c9c4fc01061881cb58a66b8eae35f437338
Asante Adarkwa Usman <asante.au96@gmail.com>
2021-11-16 09:59:50 +0000

    Added main file

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Finding the sum of 2 integers
+  print(sum(80, 10));
+}
+
+int sum(int firstNum, int secondNum) {
+  return firstNum + secondNum;
+}
+
+int Sum() {
+  var firstNum;
+  var secondNum;
+  return firstNum + secondNum;
+}
