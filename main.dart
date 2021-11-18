3950fe3118b1e2133cf1f93cf03b2030795ae4b4
Asante Adarkwa Usman <asante.au96@gmail.com>
2021-11-18 15:29:13 +0000

    Introduction to dart

--- main.dart
+++ main.dart
@@ -1,14 +1,286 @@
 void main() {
-  // Finding the sum of 2 integers
-  print(sum(80, 10));
-}
+  // //Working with variables
+  // const name = "Lina";
+  // String firstname = 'Kofi';
+  // //reassigning a value to name variables
+  // name = "Asante";
+  // print(name);
 
-int sum(int firstNum, int secondNum) {
-  return firstNum + secondNum;
-}
+  // final age = 23;
+  // age = 70;
+  // print(age);
 
-int Sum() {
-  var firstNum;
-  var secondNum;
-  return firstNum + secondNum;
+  // int numberOfPeople = 60;
+  // numberOfPeople = 100;
+  // int sum = numberOfPeople + 50;
+  // print(sum);
+
+  // //Working with Strings
+  // String myFirstName = "Asante";
+  // String myLastName = "Usman";
+  // String myFullName = myFirstName + " " + myLastName;
+  // print(myFullName);
+
+  // int age = 5;
+
+  // int sum = 5 + 10;
+
+  // double multiplication = 3.90 * 2.5;
+
+  // double division = (10 / 2);
+
+  // int modulus = 10 % 3;
+//If statements
+  int age = 20;
+  if (age >= 18) {
+    print('You are an adult');
+  }
+
+  //if else statements
+  int age2 = 20;
+  if (age2 >= 18) {
+    print('You are an adult');
+  } else {
+    print('You are not an adult');
+  }
+
+  //Nested if else statements
+  int marks = 90;
+  if (marks < 40) {
+    print('Chairman, you have failed');
+  } else if (marks <= 50) {
+    print('Charlie, you nearly failed');
+  } else if (marks <= 75) {
+    print('You are doing well');
+  } else {
+    print('Chairman, you do all');
+  }
+// Switch statements
+  int marks2 = 90;
+  switch (marks2) {
+    case 90:
+      print('You are an excellent student');
+      break;
+    case 80:
+      print('You are a good student');
+      break;
+    case 70:
+      print('You are a student');
+      break;
+    case 60:
+      print('You are a student');
+      break;
+    default:
+      print('You are a student');
+  }
+
+//Functions
+  void sayHello() {
+    print('Hello, Mr Man');
+  }
+
+  sayHello();
+
+  int sum(firstNumber, secondNumber, thirdNumber) {
+    return firstNumber + secondNumber + thirdNumber;
+  }
+
+  print(sum(6, 8, 9));
+
+  // void sayHelloTo(String name) {
+  //   print('Hello $name');
+  // }
+
+  // sayHelloTo('Lina');
+
+  // void sayHelloTo(String name, [int age]) {
+  //   print('Hello $name');
+  //   if (age != null) {
+  //     print('You are $age years old');
+  //   }
+  // }
+
+  // sayHelloTo('Lina', 23);
+
+  // void sayHelloTo(String name, [int age = 18]) {
+  //   print('Hello $name');
+  //   if (age != null) {
+  //     print('You are $age years old');
+  //   }
+  // }
+
+  // sayHelloTo('Lina', 23);
+
+  // void sayHelloTo(String name, [int age = 18]) {
+  //   print('Hello $name');
+  //   if (age != null) {
+  //     print('You are $age years old');
+  //   }
+  // }
+
+  // sayHelloTo('Lina');
+
+  // void sayHelloTo(String name, [int age = 18]) {
+  //   print('Hello $name');
+  //   if (age != null) {
+  //     print('You are $age years old');
+  //   }
+  // }
+
+  // sayHelloTo('Lina', 23);
+
+  // void sayHelloTo(String name, [int age = 18]) {
+  //   print('Hello $name');
+  //   if (age != null) {
+  //     print('You are $age years old');
+  //   }
+  // }
+
+  // sayHelloTo('Lina');
+
+  // void sayHelloTo(String name, [int age = 18]) {
+  //   print('Hello $name');
+  //   if (age != null) {
+  //     print('You are $age years old');
+  //   }
+  // }
+
+  // sayHelloTo('Lina', 23);
+
+  // void sayHello
 }
+//Modules to cover
+//1.Data Types
+//2.Variables
+//3.Operators/Operator Precedence
+//4.Conditional Statements
+//5.Loops
+//6.Functions
+//7.Lists
+//8.Maps
+//OOP(Object Oriented Programming)
+//9.Classes
+//10.Objects
+//11.Methods
+//12.Inheritance
+//13.Constructors
+
+
+//Comments
+//1.Single Line Comments(//)
+//2.Multi Line Comments(/* */)
+/* 
+Today, we'll discuss about the different types of comments in Dart.
+they are:
+1.Single Line Comments
+2.Multi Line Comments
+*/
+
+//Data Types
+//1.Numbers
+//2.Strings
+//3.Boolean
+//4.List/Array
+//5.Map
+
+//1.Numbers
+//Numbers are the most basic data type in Dart.
+//They can be of any of the following types:
+//1.int: for integer values(whole numbers)
+//2.double: for floating point values(decimal numbers)
+//3.num: for both int and double
+
+//2.Strings
+//Strings are the most basic data type in Dart.
+//They are immutable and can be of any of the following types:
+//1.String: for character values
+//An example of a string literal is:
+//'Hello World'
+//"Hello World"
+//'A'
+//"2"
+//"3.14"
+//"true"
+// String Concatenation
+//"Hello" + "World"
+//"Hello" + " " + "World"
+
+//3.Boolean
+//Boolean is the most basic data type in Dart.
+//They are immutable and can be of any of the following types:
+//1.bool: for true or false values
+//An example of a boolean literal is:
+//true
+//false
+//bool isLoading = true;
+
+
+//4.List/Array
+//List/Array is the most basic data type in Dart.
+//They are mutable and can be of any of the following types:
+//1.List: for a list of values
+//An example of a list literal is:
+//[1, 2, 3]
+//List ages = [20, 21, 22];
+
+//5.Map
+//Map is the most basic data type in Dart.
+//They are mutable and can be of any of the following types:
+//1.Map: for a list of values
+//An example of a map literal is:
+//{'name': 'John', 'age': 30}
+
+//6.Dynamic
+//Dynamic is the most basic data type in Dart.
+//They are mutable and can be of any of the following types:
+//1.dynamic: for a list of values
+//An example of a dynamic literal is:
+//dynamic name = 'John';
+
+//Variables
+/*1.Declaring Variables
+ Syntax for declaring a variable is:
+ data type variableName;
+ String name;
+ int age;
+ dynamic name;
+ var name;
+
+ Initializing a variable
+  Syntax for initializing a variable is:
+
+  int age = 20; //declaring and initializing a variable
+  int age, price, cost; //only declaring the variable
+  age = 50; // only initializing the variable
+
+//Operators in dart 
+//1.Assignment Operator(=)
+//2.Arithmetic Operators(+, -, *, /, %)
+//3.Logical Operators(&&, ||, !, <, >, <=, >=)
+
+//AND(&&)
+int age = 20;
+(age >= 18) && (age > 10)
+
+//OR(||)
+int age = 20;
+(age <= 18) || (age < 10)
+
+20 == 20
+20 != 21
+
+
+//Decision Statements
+1.if statements
+2.if else statements
+
+
+
+
+
+
+
+
+
+
+ */
